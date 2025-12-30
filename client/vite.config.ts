97be71611cb9e64d3e8c4badf5897c854e4365ec
Daniel Alvarez <daniel.alvarez.q@gmail.com>
2025-12-30 16:13:27 +0100

    Docker config for both client and server images completed and tested

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
+  preview:{
+    port:3000,
+    strictPort: true,
+  },
+  server:{
+    port:3000,
+    strictPort:true,
+  }
 })
