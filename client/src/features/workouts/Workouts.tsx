99777b4a81e4ebb5b8e7d9d4a91cb45c2d261f08
= <=>
2026-02-17 11:05:03 +0100

    Completed Tanstack Query implementation for Workouts view, along with a redesign of the component that separates different elements into distinct components

--- client/src/features/workouts/Workouts.tsx
+++ client/src/features/workouts/Workouts.tsx
@@ -1,144 +1,13 @@
-import { useState, useEffect, type ReactEventHandler} from "react"
-import type { Workout, WorkoutCreate } from "./workouts.types"
-import axios from "axios"
-import { useAuth } from "../../context/AuthContext"
+import { useState} from "react"
 import WorkoutList from "./views/WorkoutList"
 import Card from "../../shared/components/Card"
-import EventMessage from "../../shared/components/EventMessage"
-import Popup from "../../shared/components/Popup"
+import WorkoutCreateForm from "./views/WorkoutCreateForm"
 
 function Workouts(){
     //Data-bounded states
     const [error, setError] = useState<string|null>(null)
-    // const [workouts, setWorkouts] = useState<Workout[]|null>(null)
-    const [newWorkout, setNewWorkout] = useState<WorkoutCreate>(
-        {
-            name:'',
-            description:'',
-            difficulty:''
-        }
-    )
     const [creatingWorkout, setCreatingWorkouts] = useState<boolean>(false)
-    const {axios_instance} = useAuth()!
-
-    //Effects and data load
-    // const fetchWorkouts = ()=>{
-    //     setError(null)
-    //     axios_instance.get('api/workouts')
-    //     .then(response => {
-    //         setWorkouts(response.data);
-    //     })
-    //     .catch(error => {
-    //         console.error('Error feching data: ', error);
-    //     });
-    // }
-    
-    // useEffect(() => {fetchWorkouts()},[])
-
-    //Event handlers 
-    const handleWorkoutSubmit = async (e:React.FormEvent)=>{
-        e.preventDefault();
-        setError(null)
-        try{
-            const response = await axios_instance.post('api/workouts',newWorkout);
-            console.log(response.data);
-            setCreatingWorkouts(!creatingWorkout);
-            setNewWorkout({
-                name:'',
-                description:'',
-                difficulty:''
-            });
-            // fetchWorkouts();
-        }catch(error){
-            if (axios.isAxiosError(error)){
-                console.log(`Error! ${error.response?.data}`);
-                setError(JSON.stringify(error.response?.data))
-            }else{
-                setError(`Error: ${error}`)
-            }
-        }
-    }
-
-    const handleFormState = () =>{
-        let state:boolean = false
-        if(!newWorkout.name.length || !newWorkout.difficulty.length){
-            state=true
-        }
-        return state
-    }
-
-    //Visual elements
-    // const workout_list = (data:Workout[]) => {
-    //     if (data?.length){
-    //         let mapped_workouts = data.map(workout => 
-    //             <div key={workout.id} className="col-12 col-lg-3 custom-justify-content-center">
-    //                 <Card uri='workouts' id={workout.id} title={workout.name} footer={workout.created_at} body={workout.description} />
-    //             </div>
-    //         )
-    //         mapped_workouts.push(<div key="create-workout" className="col-12 col-lg-3 custom-justify-content-center"><Card body='Create a new workout' style="action" onClick={()=>setCreatingWorkouts(!creatingWorkout)}></Card></div>)
-    //         return mapped_workouts
-    //     }else{
-    //         return  <>
-    //             <div className="col-12 col-lg-3 custom-justify-content-center">
-    //                 <Card body='Create a new workout' style="action" onClick={()=>setCreatingWorkouts(!creatingWorkout)}></Card>
-    //             </div>
-    //         </>
-    //     }
-    // }
-
-    const workout_form = (handler:ReactEventHandler) =>{
-        return <form onSubmit={handler}>
-                    <div className="form-group">
-                        <div className="form-row">
-                            <label htmlFor="name">Workout title</label>
-                        </div>
-                        <div className="form-row">
-                            <input type="text" name="name" id="name" onChange={(e) => setNewWorkout({...newWorkout, name:e.target.value})}/>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="form-row">
-                            <label htmlFor="">Workout description</label>
-                        </div>
-                        <div className="form-row">
-                            <textarea  name="description" id="description" onChange={(e) => setNewWorkout({...newWorkout, description:e.target.value})}></textarea>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="form-row">
-                            <label htmlFor="difficulty">Difficulty</label>
-                        </div>
-                        <div className="form-row">
-                            <select name="difficulty" id="difficulty" onChange={(e) => setNewWorkout({...newWorkout, difficulty:e.target.value})}>
-                                <option value="ext">Extreme</option>
-                                <option value="hig">High</option>
-                                <option value="med">Medium</option>
-                                <option value="low">Low</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="form-row">
-                            <label htmlFor="source_url">Source url</label>
-                        </div>
-                        <div className="form-row">
-                            <input type="url" name="source_url" id="source_url" onChange={(e) => setNewWorkout({...newWorkout, source_url:e.target.value})}/>
-                        </div>
-                    </div>
-                    { error &&
-                        <div className="form-group">
-                            <div className="form-row">
-                                <EventMessage message={error} style="error compact"></EventMessage>
-                            </div>
-                        </div>
-                    }
-                    <div className="form-group">
-                        <div className="form-row">
-                            <button className="btn-full" disabled={handleFormState()}>Create</button>
-                        </div>
-                    </div>
-                </form>
-    }
+   
 
     return(
         <>
@@ -152,9 +21,7 @@ function Workouts(){
                 </div>
             </div>
             {creatingWorkout &&
-                <Popup title="New workout" onClose={()=> {setCreatingWorkouts(!creatingWorkout); setError(null)}}>
-                    {workout_form(handleWorkoutSubmit)}
-                </Popup>
+                <WorkoutCreateForm error={error} errorHandler={setError} displayFlagHandler={setCreatingWorkouts}/>
             }
         </>
     )
---
+++ client/src/features/workouts/views/WorkoutCreateForm.tsx
@@ -0,0 +1,118 @@
+import { useState } from "react"
+import { useAuth } from "../../../context/AuthContext"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import type { WorkoutCreate } from "../workouts.types"
+import EventMessage from "../../../shared/components/EventMessage"
+import Popup from "../../../shared/components/Popup"
+
+//Props type
+type WorkoutCreateProps = {
+    error:string|null,
+    errorHandler:React.Dispatch<React.SetStateAction<string | null>>,
+    displayFlagHandler:React.Dispatch<React.SetStateAction<boolean>>
+}
+
+//Component function
+function WorkoutCreateForm ({error, errorHandler, displayFlagHandler}:WorkoutCreateProps) {
+
+    //Helpers
+    const initialize_workout = ()=>{
+        return {name:'', description:'', difficulty:''}
+    }
+
+    //Data-bounded states
+    const {axios_instance} = useAuth()!
+    const [newWorkout, setNewWorkout] = useState<WorkoutCreate>(initialize_workout())
+    const queryClient = useQueryClient()
+
+    //Logic
+
+    const create_workout = async ()=>{
+        const response = await axios_instance.post('api/workouts',newWorkout).then(r => r);
+        return response.data
+    }
+
+    const workout_mutation = useMutation({
+            mutationFn: create_workout,
+            onSettled:(data)=>{
+                console.log(data)
+                queryClient.invalidateQueries({queryKey:['workouts']})
+                displayFlagHandler(false)
+            },
+            onError:(error)=>{
+                console.log(error)
+                errorHandler(error.message)
+            },
+
+        })
+
+    const handleWorkoutSubmit = async (e:React.FormEvent)=>{
+        e.preventDefault();
+        workout_mutation.mutate()
+        
+    }
+
+    const handleFormState = () =>{
+        let state:boolean = false
+        if(!newWorkout.name.length || !newWorkout.difficulty.length){
+            state=true
+        }
+        return state
+    }
+
+    return <Popup title="New workout" onClose={()=> {displayFlagHandler(false); errorHandler(null)}}>   
+                <form onSubmit={handleWorkoutSubmit}>
+                    <div className="form-group">
+                        <div className="form-row">
+                            <label htmlFor="name">Workout title</label>
+                        </div>
+                        <div className="form-row">
+                            <input type="text" name="name" id="name" onChange={(e) => setNewWorkout({...newWorkout, name:e.target.value})}/>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <div className="form-row">
+                            <label htmlFor="">Workout description</label>
+                        </div>
+                        <div className="form-row">
+                            <textarea  name="description" id="description" onChange={(e) => setNewWorkout({...newWorkout, description:e.target.value})}></textarea>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <div className="form-row">
+                            <label htmlFor="difficulty">Difficulty</label>
+                        </div>
+                        <div className="form-row">
+                            <select name="difficulty" id="difficulty" onChange={(e) => setNewWorkout({...newWorkout, difficulty:e.target.value})}>
+                                <option value="ext">Extreme</option>
+                                <option value="hig">High</option>
+                                <option value="med">Medium</option>
+                                <option value="low">Low</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <div className="form-row">
+                            <label htmlFor="source_url">Source url</label>
+                        </div>
+                        <div className="form-row">
+                            <input type="url" name="source_url" id="source_url" onChange={(e) => setNewWorkout({...newWorkout, source_url:e.target.value})}/>
+                        </div>
+                    </div>
+                    { error &&
+                        <div className="form-group">
+                            <div className="form-row">
+                                <EventMessage message={error} style="error compact"></EventMessage>
+                            </div>
+                        </div>
+                    }
+                    <div className="form-group">
+                        <div className="form-row">
+                            <button className="btn-full" disabled={handleFormState()}>Create</button>
+                        </div>
+                    </div>
+                </form>
+            </Popup>
+}
+
+export default WorkoutCreateForm
