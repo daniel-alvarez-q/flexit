a01e63e946fc3ddcee4ab1b177381e807550a7f1
Daniel Alvarez <daniel.alvarez.q@gmail.com>
2026-08-22 20:10:49 +0200

    Initial logic for delete exercise, with pending logic for handling redirection to previous view

--- client/src/features/exercise/views/ExerciseDelete.tsx
+++ client/src/features/exercise/views/ExerciseDelete.tsx
@@ -3,6 +3,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useAuth } from "../../../context/AuthContext";
 import Popup from "../../../shared/components/Popup";
 import EventMessage from "../../../shared/components/EventMessage";
+import '../exerciseDetail.css'
 
 type ExerciseDeletePopupParams = {
     exerciseId:Number;
@@ -11,9 +12,43 @@ type ExerciseDeletePopupParams = {
 
 
 function ExerciseDeletePopup({exerciseId, displayHandler}:ExerciseDeletePopupParams){
+
+    const {axios_instance} = useAuth()!
+    const queryClient = useQueryClient()
+    const mutation = useMutation({
+        mutationFn: ()=>{
+            const response= axios_instance.delete(`api/exercise/${exerciseId}`).then(r=>r)
+            return response
+        },
+        onSuccess: async()=>{
+            await Promise.all([
+                queryClient.invalidateQueries({
+                    queryKey:['exercise', String(exerciseId)]
+                }
+            ),
+            ])
+            displayHandler(false)
+        },
+        onError: (error)=>{
+            console.log(error)
+        }
+    })
+
+    function handleDelete(){
+        mutation.mutate()
+    }
+
     return(
         <Popup title="Delete exercise" onClose={()=> displayHandler(false)}>
-            Test
+            <div className="delete-popup">
+                <div className="delete-popup-text">
+                    Delete this exercise?
+                </div>
+                <div className="delete-popup-actions">
+                    <button onClick={()=>handleDelete()}>Delete</button>
+                    <button onClick={()=>displayHandler(false)}>Cancel</button>
+                </div>
+            </div>
         </Popup>
     )
 }
